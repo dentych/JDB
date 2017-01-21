75be633022ef3ebd854ad24363d8575fea27b5c4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-01-21 22:31:57 +0100

    minor update in row creation

--- public/js/room.js
+++ public/js/room.js
@@ -39,7 +39,7 @@ socket.on("player-joined", (data) => {
 
     if (!isInBeef) {
         $(".game-overview").show();
-        $("#score-table tr:last").after('<tr>data</tr><tr></tr><tr>0</tr>');
+        $("#score-table > tbody:last-child").append('<tr><td>data.name</td><td>0</td></tr>');
     }
 });
 
