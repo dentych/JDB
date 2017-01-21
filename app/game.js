31873b4d12d5f333813ffdc0e5c4817752cbdabb
joachimda <joachimdamandersen@gmail.com>
2017-01-21 19:04:46 +0100

    added input event logic

--- app/game.js
+++ app/game.js
@@ -12,7 +12,37 @@ module.exports = function (io, aliasGenerator) {
         });
 
         socket.on("input", (data) => {
+            let playerIndex = nowPlaying.indexOf(socket);
+            let player2Index = playerIndex == 0 ? 1 : 0;
 
+            if (data == "hate") {
+                nowPlaying[playerIndex].answer = data;
+            } else if (data == "love") {
+                nowPlaying[playerIndex].answer = data;
+            }
+
+            if (nowPlaying[playerIndex].answer != null && nowPlaying[player2Index].answer != null){
+                console.log("Both players has answered...");
+                let shotsConsumed = [];
+                if (nowPlaying[playerIndex].answer == "hate" && nowPlaying[player2Index].answer == "hate"){
+                    socket.consumed += 1;
+                    nowPlaying.forEach((element) => {
+                        shotsConsumed.push({
+                            name: element.name,
+                            consumed: 1
+                        });
+                    });
+                }
+                else if (nowPlaying[playerIndex].answer == "love" && nowPlaying[player2Index].answer == "hate"){
+                    socket.consumed += 2;
+                    shotsConsumed.push({
+                        name: socket.name,
+                        consumed: 2
+                    });
+                }
+
+                io.emit("shot", shotsConsumed);
+            }
         });
 
         socket.on("current-data", (data) => {
@@ -24,3 +54,5 @@ module.exports = function (io, aliasGenerator) {
         });
     });
 };
+
+
