1379da481046072eb5e14ac1893b1916d65a4df2
joachimda <joachimdamandersen@gmail.com>
2017-01-21 15:58:41 +0100

    added generateRoomCode method to AliasGenerator class

--- app/getWords.js
+++ app/getWords.js
@@ -10,12 +10,12 @@ class WordReader {
         return fs.readFileSync(this.filePath, 'utf8');
     }
 
-    print(){
+    printAllWords(){
         console.log(this.wordArray);
     }
 }
 
-class NameGenerator{
+class AliasGenerator{
     constructor(nouns, adjectives) {
         this.nounArray = nouns;
         this.adjectiveArray = adjectives;
@@ -24,13 +24,24 @@ class NameGenerator{
             var randomName = this.adjectiveArray[Math.floor(Math.random() * this.adjectiveArray.length)];
             return randomName += " " + this.nounArray[Math.floor(Math.random() * this.nounArray.length)];
         }
-}
 
+        generateRoomCode(){
+            var text = "";
+            var rep = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+            for( var i = 0; i < 5; i++)
+                text += rep.charAt(Math.floor(Math.random() * rep.length));
+            return text;
+        }
+}
 
 const nounReader = new WordReader('../appData/nounlist.txt');
 const adjReader = new WordReader('../appData/adjectiveList.txt');
 
-const gen = new NameGenerator(nounReader.wordArray, adjReader.wordArray);
+const gen = new AliasGenerator(nounReader.wordArray, adjReader.wordArray);
 
 const player1 = gen.generatePlayerName();
+const game1 = gen.generateRoomCode();
+
 console.log(player1);
+console.log(game1);
