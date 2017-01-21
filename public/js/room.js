10387cfe03b5a6f5b466cf551b65105cab6b980f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-01-21 18:38:43 +0100

    client event handling wip

--- public/js/room.js
+++ public/js/room.js
@@ -18,7 +18,43 @@ $(document).ready(function () {
 });
 
 var socket = io.connect(window.location.protocol + "//" + window.location.hostname + ":" + window.location.port);
+
 socket.on("username", (data) => {
     console.log(data);
     $("#player-name")[0].innerText = data;
+    HideAllViews();
+    $(".waiting-for-players").show();
 });
+
+socket.on("player-joined", (data) => {
+    console.log(data);
+    HideAllViews();
+    $(".game-overview").show();
+    $("#score-table tr:last").after('<tr>data</tr><tr></tr><tr>0</tr>');
+});
+
+socket.on("player-leave", (data) => {
+    console.log(data);
+    HideAllViews();
+});
+
+socket.on("beef", (data) => {
+    console.log(data);
+    HideAllViews();
+    $(".playing").show();
+
+});
+
+socket.on("shot", (data) => {
+    console.log(data);
+    HideAllViews();
+    $(".game-overview").show();
+});
+
+socket.on("current-data", (data) => {
+    console.log(data);
+});
+
+function HideAllViews() {
+    $("div.card > div").hide();
+}
